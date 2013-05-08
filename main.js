f899f95f10af22dca5adf7fb65947a5a5212966d
vadim <stogrammist@gmail.com>
2013-05-08 14:49:34 +0000

    + figures

--- main.js
+++ main.js
@@ -1,18 +1,105 @@
 $(document).ready(function()
 {
-	var field = new Field(300);
-	field.setCellStatus(5,10,1);
-	field.setCellStatus(5,11,1);
-	field.setCellStatus(5,9,1);
-	field.setCellStatus(6,9,1);
+	var game = new Game(300);
+	game.start();
 })
 
-function Field(rWidth)
+
+Game = function(rWidth)
+{
+	gameField = []
+	width = 10;
+	height = width * 2;
+	field = new Field(rWidth, width);
+	speed = 500; // One update per second
+
+	for(var i = 0; i < width; i++)
+	{
+		gameField[i] = []
+		for(var j = 0; j < height; j++)
+		{
+			gameField[i][j] = 0;
+		}
+	}
+	gameField[4][4] = 1;
+	gameField[4][5] = 1;
+	gameField[4][8] = 1;
+	gameField[4][9] = 1;
+
+
+	this.start = function()
+	{
+		setInterval(update, speed);
+		setInterval(draw, 20);
+	}
+
+	function update()
+	{
+		//Figure.draw();
+		for(var j = height - 1; j >= 0; j--)
+		{
+			for(var i = 0; i < width; i++)
+			{
+				if(gameField[i][j] == 1 && gameField[i][j+1] != 2 && j < height - 1)
+				{
+					gameField[i][j] = 0;
+					gameField[i][j+1] = 1;
+				}
+				else if(gameField[i][j] == 1)
+				{
+					gameField[i][j] = 2;
+				}
+			}
+		}
+	}
+
+	function draw()
+	{
+		for(var i = 0; i < width; i++)
+		{
+			for(var j = 0; j < height; j++)
+			{
+				field.setCellStatus(i, j, gameField[i][j]);
+			}
+		}
+	}
+
+	Figure = 
+	{
+		position: [width / 2, 0],
+		form: [[1, 1, 0],
+				[0, 1, 0],
+				[0, 1, 0]],
+
+		create: function()
+		{
+
+		},
+
+		moveDown: function()
+		{
+			this.position[1]++;
+		},
+
+		draw: function()
+		{
+			for(var i = 0; i < 3; i++)
+			{
+				for(var j = 0; j < 3; j++)
+				{
+					gameField[this.position[0] + i, this.position[1] + j] = this.form[i,j];
+				}
+			}
+		}
+	}
+}
+
+Field = function(rWidth, fWidth)
 {
 	this.realWidth = rWidth;
 	this.realHeight = this.realWidth * 2;
-	this.width = 10;
-	this.height = 20;
+	this.width = fWidth;
+	this.height = fWidth * 2;
 	this.paper = Raphael(10, 50, this.realWidth, this.realHeight);
 	this.wMult = this.realWidth/this.width; 
 	this.hMult = this.realHeight/this.height;
@@ -29,9 +116,13 @@ function Field(rWidth)
 
 	this.setCellStatus = function(x, y, status)
 	{
-		if(status)
+		if(status == 1)
 		{
 	 		this.fieldArray[x][y].attr("fill", "#f00");
+	 	}
+	 	else if(status == 2)
+	 	{
+	 		this.fieldArray[x][y].attr("fill", "#00f");
 	 	}
 	 	else
 	 	{
